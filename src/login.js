343f5585d521dc59e1d76c14d271199a3d74e2d5
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-05-31 09:57:25 -0500

    added password

--- src/login.js
+++ src/login.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            email: ""
+
+    state = {
+        email: "",
+        password: "",
+    }
+
+    validateForm = (evt) => {
+        evt.preventDefault()
+        if (this.state.email.length > 0) {
+            return fetch(`http://localhost:8080/users?email=${this.state.email}&${this.state.password}`)
+                .then((response) => {
+                    return response.json();
+                    // user.forEach(currentUser => {
+
+                        // if (currentUser.email === this.state.email) {
+                        //     sessionStorage.setItem("ActiveUser", JSON.stringify({user}))
+                        // }
+                    // })
+                }).then((user) => {
+                    console.log(user[0])
+                });
         }
     }
-    validateForm= function (evt) {
-        evt.preventDefault() 
-        if (this.state.email.length > 0) {
-            return fetch ("http://localhost:8088/users")
-            .then(
-                user => {
-                    console.log(user)
-                // user.forEach(currentUser => {
-                    
-                    // if (currentUser.email === this.state.email) {
-                    //     sessionStorage.setItem("ActiveUser", JSON.stringify({user}))
-                    // }
-                // })
-            }
-            )
-        }
-    }.bind(this)
 
-    handleFormFieldChange = function (evt) {
-        const stateToChange = {}
-        stateToChange[evt.target.id] = evt.target.value
-        this.setState(stateToChange)
-    }.bind(this)
+    handleEmailChange = (evt) => {
+        this.setState({email: evt.target.value})
+    }
+
+    handlePasswordChange = (evt) => {
+        this.setState({password: evt.target.value})
+    }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.validateForm}>
-                <input type="text" id="email" value={this.state.email} placeholder="Email" onChange={this.handleFormFieldChange} />
-                <input type="submit" />
+                <input type="text" id="email" value={this.state.email} placeholder="Email" onChange={this.handleEmailChange} />
+                <input type="password" id="password" value={this.state.password} onChange={this.handlePasswordChange} placeholder="Password"  />
+                <input type="submit" value="Submit"/>
                 </form>
             </div>
         )
