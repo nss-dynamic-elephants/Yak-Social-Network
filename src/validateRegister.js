840581edab1639581308b22e8e89343abc091681
Deanna Vickers <deanna2000@gmail.com>
2018-05-31 13:50:45 -0500

    completed register form and added basic structure for validate module

---
+++ src/validateRegister.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+class ValidateRegister extends Component {
+
+
+    render() {
+        return (
+          <div className="validate">
+                <button className="submitButton">Submit</button>
+            <p>
+                Validate
+            </p>
+          </div>
+
+      );
+    }
+  }
+  export default ValidateRegister;
